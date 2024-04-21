1e3d9c4cdc6edf164ff012d2e6584888fb603240
muhammadazizbek006 <umarovxaydaraliismonov@gmail.com>
2024-04-21 12:29:04 +0300

    d

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,8 +15,8 @@ const Home = () => {
     <StamatalogikXizmatlar/>
     <OmmabopXizmatlar/>
     <KlinikaShfokorlari/>
-    {/* <KopSavollar/> */}
-    {/* <KlinikaXaqida/> */}
+    <KopSavollar/>
+    <KlinikaXaqida/>
     {/* <Cta/> */}
     </>
   )
