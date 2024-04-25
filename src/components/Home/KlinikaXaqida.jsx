368429479dfd0fece613b08e127f8a35fd275ee6
muhammadazizbek006 <umarovxaydaraliismonov@gmail.com>
2024-04-25 16:48:03 +0300

    add klinika xaqida responsive

--- src/components/Home/KlinikaXaqida.jsx
+++ src/components/Home/KlinikaXaqida.jsx
@@ -4,12 +4,12 @@ const KlinikaXaqida = () => {
   return (
     <section className='xaqida py-16'>
         <div className="containerb">
-        <h2 className='text-42 mb-12'>Klinika haqida</h2>
+        <h2 className='text-42 mb-10 lg:mb-3 text-center lg:text-start'>Klinika haqida</h2>
 
-        <ul className='flex justify-between items-center'>
+        <ul className='flex flex-col lg:flex-row text-center lg:text-start   justify-between items-center'>
             
             {/* left */}
-            <li className='text-base max-w-[633px]'>
+            <li className=' text-sm xl:text-base max-w-[500px] xl:max-w-[633px]'>
                 <p className='mb-10'>
                 Clinica Native terapevtik, protez va profilaktik stomatologiyani o'z ichiga olgan keng ko'lamli stomatologik xizmatlarni taklif etadi. Biz  bemorlarimizga har qanday vaziyatda yordam berish uchun zamonaviy materiallar va davolash usullaridan foydalanamiz.</p>
                 <p>Kariyes, kariyesning murakkab shakllari, karioz bo'lmagan shikastlanishlar, tishlarni tiklash, endodontik davolash, tishlarni oqartirish va yallig'lanishli periodontal kasalliklarni konservativ davolash.
@@ -18,8 +18,9 @@ const KlinikaXaqida = () => {
                 Vektor Paro Pro qurilmalari va Doctor Smile diodli lazer yordamida yallig'lanishli periodontal kasalliklarni davolash.</p>
                 <p>Davolash rejasi kasallikning klinik ko'rinishlariga, bemorning yoshiga va boshqa omillarga qarab mutaxassis tomonidan individual ravishda tanlanadi.</p>
             </li>
-            <li>
-                 <video controls className="mt-4 w-[656px] h-[369px] inline-block">
+            <li className='hidden sm:inline-block'>
+                
+                 <video controls className="mt-4 w-[400px] xl:w-[606px] h-[369px] inline-block">
                   <source src="your-video-url.mp4" type="video/mp4" />
                   Your browser does not support the video tag.
                 </video>
