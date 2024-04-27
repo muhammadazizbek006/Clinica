eb610d6147cc3fa11edc8627b5608ae38431dab2
muhammadazizbek006 <umarovxaydaraliismonov@gmail.com>
2024-04-27 09:23:59 +0300

    add periadanatalogiya page to responsive

--- src/pages/periAdanatalogiya/PeriAdanatalogiya.jsx
+++ src/pages/periAdanatalogiya/PeriAdanatalogiya.jsx
@@ -14,9 +14,9 @@ const PeriAdanatalogiya = () => {
       <section>
         <div className="containerb">
           {/* top */}
-          <div className=" flex items-center  pt-5">
+          <div className=" flex justify-center sm:justify-start items-center  pt-5">
             {/* left */}
-            <div className="flex items-center">
+            <div className="flex flex-col sm:flex-row  items-center">
               <div className="w-28 bg-akva  mr-6 ">
                 <img className="  px-5 py-3  " src={peri} alt="#" />
               </div>
@@ -42,11 +42,11 @@ const PeriAdanatalogiya = () => {
                 >
                   <Link
                     to={`/periadanatalog/${e.yonalishi}`}
-                    className="flex justify-between items-center p-5"
+                    className="flex  flex-col justify-between md:items-center md:flex-row p-5"
                   >
                     {/* left */}
                     <div className="flex flex-col">
-                      <h3 className="text-xl font-medium mb-3 max-w-[900px]">
+                      <h3 className=" text-base lg:text-xl font-medium mb-3 max-w-[900px]">
                         {e.yonalishi}
                       </h3>
                       <div className="flex items-center  ">
@@ -58,7 +58,7 @@ const PeriAdanatalogiya = () => {
                     </div>
                     {/* right */}
                     <div className="flex items-center">
-                      <p className="mr-12 text-lg ">{e.narxi} rubldan</p>
+                      <p className=" hidden lg:block mr-12 text-lg ">{e.narxi} rubldan</p>
                       <div className="flex items-center text-lg">
                         <p className="mr-2">{e.soni}</p>
                       </div>
