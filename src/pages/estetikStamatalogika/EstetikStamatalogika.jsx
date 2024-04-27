bb9a72fd355f56d3b950b81ec335d63676a210cc
muhammadazizbek006 <umarovxaydaraliismonov@gmail.com>
2024-04-27 08:39:46 +0300

    add estetik responsive

--- src/pages/estetikStamatalogika/EstetikStamatalogika.jsx
+++ src/pages/estetikStamatalogika/EstetikStamatalogika.jsx
@@ -13,13 +13,13 @@ const EstetikStamatalogika = () => {
     <section>
       <div className="containerb">
         {/* top */}
-        <div className=' flex items-center  pt-5'>
+        <div className=' flex items-center justify-center sm:justify-start  pt-5'>
           {/* left */}
-          <div className='flex items-center'>
+          <div className='flex flex-col sm:flex-row items-center'>
             <div className='w-28 bg-akva  mr-6 '>
               <img className='  px-5 py-3  ' src={estetik} alt='#' />
             </div>
-            <h2 className='text-32 font-medium border-b-4 border-b-header w-full '>Estetik stomatologiya</h2>
+            <h2 className=' text-xl sm:text-32 font-medium border-b-4 border-b-header w-full '>Estetik stomatologiya</h2>
           </div>
           {/* right */}
         </div>
@@ -34,10 +34,10 @@ const EstetikStamatalogika = () => {
                     Estetik.map((e)=>{
                         return(
                             <li key={e.id} className='border-b-2 border-b-akva border-dashed hover:bg-sky-50 duration-300'>
-                                <Link  to={`/estetika/${e.yonalishi}`} className='flex justify-between items-center p-5'>
+                                <Link  to={`/estetika/${e.yonalishi}`} className='flex flex-col sm:flex-row justify-between sm:items-center p-5'>
                                     {/* left */}
                                     <div className='flex flex-col'>
-                                    <h3 className='text-xl font-medium mb-3 max-w-[900px]'>{e.yonalishi}</h3>
+                                    <h3 className=' text-lg sm:text-xl font-medium mb-3 max-w-[900px]'>{e.yonalishi}</h3>
                                     <div className='flex items-center  '>
                                         <p className='mr-6 text-xl text-next border-b border-b-cyan-700'>Batafsil</p>
                                         <img src={next} alt="#" />
@@ -45,7 +45,7 @@ const EstetikStamatalogika = () => {
                                     </div>  
                                     {/* right */}
                                     <div className='flex items-center'>
-                                    <p className='mr-12 text-lg '>{e.narxi} rubldan</p>
+                                    <p className='mr-12 text-lg  hidden md:block '>{e.narxi} rubldan</p>
                                     <div className='flex items-center text-lg'>
                                     <p className='mr-2'>{e.soni}</p>
                                     </div>
