a73bb4f7dcb2cca8bbd54c6862e28e8afbbc5c7c
muhammadazizbek006 <umarovxaydaraliismonov@gmail.com>
2024-04-28 14:22:54 +0300

    add kontaktlar responsive

--- src/pages/kontaktlar/Kontaktlar.jsx
+++ src/pages/kontaktlar/Kontaktlar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Kontaktlar = () => {
   return (
     <>
-        <section>
+        <section className='py-8'>
             <div className="containerb">
                 {/* title */}
                 <h2 className='text-42 mb-8'>Kontaktlar</h2>
@@ -20,7 +20,7 @@ const Kontaktlar = () => {
                         <strong>Telefon:</strong>
                         <p> +7 (343) 343 77 00</p>
                     </li>
-                    <li  className='flex items-center space-x-2'>
+                    <li  className='flex flex-col text-start justify-start items-start sm:flex-row sm:space-x-2'>
                         <strong>Pochta:</strong>
                         <a href="email">umarovmuxammadazizbek20@gmail.com</a>
                     </li>
