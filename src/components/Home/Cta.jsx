7d8c67fd918b8d5449d64edd590de4bb0289e378
muhammadazizbek006 <umarovxaydaraliismonov@gmail.com>
2024-04-29 23:18:21 +0300

    add h-full

--- src/components/Home/Cta.jsx
+++ src/components/Home/Cta.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Cta = () => {
   return (
     <div className="relative flex justify-center items-center h-full py-10">
-      <iframe  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3016.7974454966748!2d71.97460797585151!3d40.87631917136938!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38bca14b5018fdcb%3A0xedfc2c995d1b9641!2sWebKing!5e0!3m2!1sru!2s!4v1711367706184!5m2!1sru!2s" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" className=" absolute w-full  top-0 left-0"></iframe>
+      <iframe  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3016.7974454966748!2d71.97460797585151!3d40.87631917136938!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38bca14b5018fdcb%3A0xedfc2c995d1b9641!2sWebKing!5e0!3m2!1sru!2s!4v1711367706184!5m2!1sru!2s" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" className=" absolute w-full h-full top-0 left-0"></iframe>
       {/* kontakt */}
       <div className="containerb z-40 hidden  md:block">
           <div className=" bg-white h-64 w-[562px] p-6 ">
