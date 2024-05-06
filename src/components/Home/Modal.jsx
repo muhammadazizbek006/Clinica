a80b19ed9aff6ba44bb2ed9a7622decf5c3fcb9b
muhammadazizbek006 <umarovxaydaraliismonov@gmail.com>
2024-05-06 16:55:12 +0300

    fix

--- src/components/Home/Modal.jsx
+++ src/components/Home/Modal.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import { useEffect } from "react";
 import axios from "axios";
 const Modal = () => {
   const [activeTab, setActiveTab] = useState(1);
